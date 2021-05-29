71c3034cf9eae31bcf42e91cfd29c8a65694ae81
Rafiuth <daf17a507a@gmail.com>
2021-05-29 01:51:48 -0300

    TransformPipe parser

--- AnvilPacker/Encoder/Transforms/TransformPipe.cs
+++ AnvilPacker/Encoder/Transforms/TransformPipe.cs
@@ -1,52 +1,37 @@
+#nullable enable
+
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using AnvilPacker.Level;
+using AnvilPacker.Util;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace AnvilPacker.Encoder.Transforms
 {
-    public class TransformPipe
+    public partial class TransformPipe
     {
-        public static TransformPipe Empty { get; } = new();
+        public static TransformPipe Empty { get; } = new(Enumerable.Empty<TransformBase>());
 
-        //Pipe string: hidden_block_removal[samples=64,radius=3,cum_freqs=false],!predict_upgrade_data
         public IReadOnlyList<TransformBase> Transforms { get; }
 
-        public TransformPipe(params TransformBase[] transforms)
+        public TransformPipe(IEnumerable<TransformBase> transforms)
         {
-            Transforms = transforms;
+            Transforms = transforms.ToList();
         }
 
         public void Apply(RegionBuffer region)
         {
-            throw new NotImplementedException();
+            foreach (var transform in Transforms) {
+                transform.Apply(region);
+            }
         }
         public void Reverse(RegionBuffer region)
         {
-            throw new NotImplementedException();
-        }
-
-        public static void RegisterTransforms()
-        {
-            Register<HiddenBlockRemovalTransform>("hidden_block_removal")
-                .AddSetting("samples",      t => ref t.Samples)
-                .AddSetting("radius",       t => ref t.Radius)
-                .AddSetting("cum_freqs",    t => ref t.CummulativeFreqs);
-
-            Register<UpgradeDataTransform>("predict_upgrade_data");
-        }
-
-        private static RegisteredTransform<T> Register<T>(string name) where T : TransformBase
-        {
-            return null;
-        }
-
-        class RegisteredTransform<T> where T : TransformBase
-        {
-            public delegate ref V RefGetter<V>(T val);
-
-            public RegisteredTransform<T> AddSetting<V>(string name, RefGetter<V> field)
-            {
-                return this;
+            foreach (var transform in Transforms.Reverse().OfType<ReversibleTransform>()) {
+                transform.Reverse(region);
             }
         }
     }
