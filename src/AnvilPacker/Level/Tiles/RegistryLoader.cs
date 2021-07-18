f3a21cbae5f653538d5b34704cfbd49d668acb07
Rafiuth <daf17a507a@gmail.com>
2021-07-17 23:19:16 -0300

    Use System.Text.Json to load block registry
    - It's about 3x faster than Newtonsoft.Json

--- src/AnvilPacker/Level/Tiles/RegistryLoader.cs
+++ src/AnvilPacker/Level/Tiles/RegistryLoader.cs
@@ -4,9 +4,9 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using AnvilPacker.Data;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+using AnvilPacker.Util;
 using NLog;
 
 namespace AnvilPacker.Level
@@ -30,20 +30,18 @@ namespace AnvilPacker.Level
 
         private static void LoadBlocks()
         {
-            using var reader = new JsonTextReader(new StreamReader(GetResourcePath("blocks.json"), Encoding.UTF8));
-            JObject json = JObject.Load(reader);
-
-            var arr = (JArray)json["blocks"];
+            using var json = LoadJson("blocks.json");
+            var arr = json.RootElement.GetProperty("blocks");
 
             var propCache = new HashSet<BlockProperty>();
 
-            foreach (var jb in arr) {
-                var name = ResourceName.Parse(jb["name"].Value<string>());
-                int minStateId = jb["minStateId"].Value<int>();
-                int maxStateId = jb["maxStateId"].Value<int>();
-                int defaultStateId = jb["defaultStateId"].Value<int>();
-                var props = ParseProperties(jb["properties"], propCache);
-                var materialName = jb["material"].Value<string>();
+            foreach (var jb in arr.EnumerateArray()) {
+                var name = ResourceName.Parse(jb.GetString("name"));
+                int minStateId = jb.GetInt("minStateId");
+                int maxStateId = jb.GetInt("maxStateId");
+                int defaultStateId = jb.GetInt("defaultStateId");
+                var props = ParseProperties(jb.GetProperty("properties"), propCache);
+                var materialName = jb.GetString("material");
 
                 var states = new BlockState[maxStateId - minStateId + 1];
                 var block = new Block() {
@@ -54,13 +52,16 @@ namespace AnvilPacker.Level
                     IsKnown = true
                 };
 
-                var stateFlags = jb["states"]["flags"];
-                var stateLight = jb["states"]["light"]; //packed light info, luminance << 4 | opacity
+                var statesProp = jb.GetProperty("states");
+                var stateFlags = statesProp.GetProperty("flags");
+                var stateLight = statesProp.GetProperty("light"); //packed light info, luminance << 4 | opacity
+                bool stateFlagsIsArr = stateFlags.ValueKind == JsonValueKind.Array;
+                bool stateLightIsArr = stateLight.ValueKind == JsonValueKind.Array;
 
                 for (int id = minStateId; id <= maxStateId; id++) {
                     int i = id - minStateId;
-                    var flags = (stateFlags is JArray ? stateFlags[i] : stateFlags).Value<int>();
-                    var light = (stateLight is JArray ? stateLight[i] : stateLight).Value<int>();
+                    var flags = (stateFlagsIsArr ? stateFlags[i] : stateFlags).GetInt32();
+                    var light = (stateLightIsArr ? stateLight[i] : stateLight).GetInt32();
 
                     states[i] = new BlockState() {
                         Id = id,
@@ -76,33 +77,19 @@ namespace AnvilPacker.Level
             }
         }
 
-        private static (string Key, string Value)[] CreatePropertyValues(List<BlockProperty> props, int stateId)
-        {
-            var vals = new (string Key, string Value)[props.Count];
-            int shift = 1, i = 0;
-            foreach (var prop in props) {
-                int valIndex = (stateId / shift) % prop.ValueCount;
-                vals[i] = (prop.Name, prop.GetValue(valIndex));
-                shift *= prop.ValueCount;
-                i++;
-            }
-            return vals;
-        }
-
         private static void LoadLegacyBlocks()
         {
-            using var reader = new JsonTextReader(new StreamReader(GetResourcePath("legacy_blocks.json"), Encoding.UTF8));
-            JObject json = JObject.Load(reader);
-            var arr = (JArray)json["blocks"];
+            using var json = LoadJson("legacy_blocks.json");
+            var arr = json.RootElement.GetProperty("blocks");
 
             var propCache = new HashSet<BlockProperty>();
 
-            foreach (var jb in arr) {
-                var name = ResourceName.Parse(jb["name"].Value<string>());
-                int id = jb["id"].Value<int>();
-                int defaultStateId = jb["defaultStateId"].Value<int>();
-                var props = ParseProperties(jb["properties"], propCache);
-                var materialName = jb["material"].Value<string>();
+            foreach (var jb in arr.EnumerateArray()) {
+                var name = ResourceName.Parse(jb.GetString("name"));
+                int id = jb.GetInt("id");
+                int defaultStateId = jb.GetInt("defaultStateId");
+                var props = ParseProperties(jb.GetProperty("properties"), propCache);
+                var materialName = jb.GetString("material");
                 var states = new BlockState[16];
 
                 var block = new Block() {
@@ -113,15 +100,19 @@ namespace AnvilPacker.Level
                     IsKnown = true
                 };
 
-                var stateFlags = jb["states"]["flags"];
-                var stateLight = jb["states"]["light"]; //packed light info, luminance << 4 | opacity
-                var stateProps = jb["states"]["states"];
+                var statesProp = jb.GetProperty("states");
+                var stateFlags = statesProp.GetProperty("flags");
+                var stateLight = statesProp.GetProperty("light"); //packed light info, luminance << 4 | opacity
+                var stateProps = statesProp.GetProperty("states");
+                bool stateFlagsIsArr = stateFlags.ValueKind == JsonValueKind.Array;
+                bool stateLightIsArr = stateLight.ValueKind == JsonValueKind.Array;
+                bool statePropsIsArr = stateProps.ValueKind == JsonValueKind.Array;
 
                 for (int m = 0; m < 16; m++) {
                     int stateId = id << 4 | m;
-                    var flags = (stateFlags is JArray ? stateFlags[m] : stateFlags).Value<int>();
-                    var light = (stateLight is JArray ? stateLight[m] : stateLight).Value<int>();
-                    var rawVals = stateProps is JArray pa ? (m < pa.Count ? pa[m].Value<string>() : null) : stateProps.Value<string>();
+                    var flags = (stateFlagsIsArr ? stateFlags[m] : stateFlags).GetInt();
+                    var light = (stateLightIsArr ? stateLight[m] : stateLight).GetInt();
+                    var rawVals = statePropsIsArr ? (m < stateProps.GetArrayLength() ? stateProps[m].GetString() : null) : stateProps.GetString();
 
                     if (stateId != defaultStateId && rawVals == null) {
                         continue; //avoid creating unecessary objects
@@ -147,10 +138,23 @@ namespace AnvilPacker.Level
             }
         }
 
-        private static List<BlockProperty> ParseProperties(JToken jprops, HashSet<BlockProperty> cache)
+        private static (string Key, string Value)[] CreatePropertyValues(List<BlockProperty> props, int stateId)
+        {
+            var vals = new (string Key, string Value)[props.Count];
+            int shift = 1, i = 0;
+            foreach (var prop in props) {
+                int valIndex = (stateId / shift) % prop.ValueCount;
+                vals[i] = (prop.Name, prop.GetValue(valIndex));
+                shift *= prop.ValueCount;
+                i++;
+            }
+            return vals;
+        }
+
+        private static List<BlockProperty> ParseProperties(JsonElement jprops, HashSet<BlockProperty> cache)
         {
             var props = new List<BlockProperty>();
-            foreach (JObject jp in jprops) {
+            foreach (var jp in jprops.EnumerateArray()) {
                 var prop = ParseProperty(jp);
                 if (!cache.TryGetValue(prop, out var cachedProp)) {
                     cache.Add(prop);
@@ -160,28 +164,41 @@ namespace AnvilPacker.Level
             }
             return props;
         }
-        private static BlockProperty ParseProperty(JObject obj)
+        private static BlockProperty ParseProperty(JsonElement obj)
         {
-            var name = obj["name"].Value<string>();
-            var type = obj["type"].Value<string>();
+            var name = obj.GetString("name");
+            var type = obj.GetString("type");
 
             switch (type) {
                 case "bool": {
                     return new BoolProperty(name);
                 }
                 case "int": {
-                    int min = obj["min"].Value<int>();
-                    int max = obj["max"].Value<int>();
+                    int min = obj.GetInt("min");
+                    int max = obj.GetInt("max");
                     return new IntProperty(name, min, max);
                 }
                 case "enum": {
-                    var values = obj["values"].ToObject<string[]>();
+                    var jvals = obj.GetProperty("values");
+                    var values = new string[jvals.GetArrayLength()];
+                    for (int i = 0; i < values.Length; i++) {
+                        values[i] = jvals[i].GetString();
+                    }
                     return new EnumProperty(name, values);
                 }
                 default: throw new NotSupportedException($"Unknown property type '{type}'");
             }
         }
 
+        private static JsonDocument LoadJson(string filename)
+        {
+            var opts = new JsonDocumentOptions() {
+                CommentHandling = JsonCommentHandling.Skip,
+                AllowTrailingCommas = true
+            };
+            using var stream = File.OpenRead(GetResourcePath(filename));
+            return JsonDocument.Parse(stream, opts);
+        }
         private static string GetResourcePath(string filename)
         {
             return Path.Combine(AppContext.BaseDirectory, "Resources", filename);
---
+++ src/AnvilPacker/Util/JsonExtensions.cs
@@ -0,0 +1,27 @@
+using System.Text.Json;
+
+namespace AnvilPacker.Util
+{
+    public static class JsonExtensions
+    {
+        public static string GetString(this in JsonElement je, string key)
+        {
+            return je.GetProperty(key).GetString();
+        }
+        public static int GetInt(this in JsonElement je, string key)
+        {
+            return je.GetProperty(key).GetInt32();
+        }
+        public static long GetLong(this in JsonElement je, string key)
+        {
+            return je.GetProperty(key).GetInt64();
+        }
+        public static double GetDouble(this in JsonElement je, string key)
+        {
+            return je.GetProperty(key).GetDouble();
+        }
+
+        public static int GetInt(this in JsonElement je) => je.GetInt32();
+        public static long GetLong(this in JsonElement je) => je.GetInt64();
+    }
+}
